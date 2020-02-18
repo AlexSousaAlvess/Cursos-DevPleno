7952303c44451b61fe31a63f24bd8513459e0311
AlexSousaAlvess <alexsousaalves317@gmail.com>
2020-02-18 20:50:32 -0300

    start course gatsby

---
+++ Gatsby/Mod01/manualmente/.cache/sync-requires.js
@@ -0,0 +1,11 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Estudos\\_Curso-DevPleno\\Gatsby\\Mod01\\manualmente\\.cache\\dev-404-page.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\Alex\\Desktop\\Estudos\\_Curso-DevPleno\\Gatsby\\Mod01\\manualmente\\src\\pages\\index.js")))
+}
+
